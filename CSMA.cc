3a68122e5a79f192dc6c99e089607c96c1159e28
Kishoremanjunath09 <116911063+Kishoremanjunath09@users.noreply.github.com>
2023-07-03 16:06:41 +0530

    Update CSMA.cc

--- CSMA.cc
+++ CSMA.cc
@@ -15,7 +15,9 @@ int main (int argc, char *argv[])
   nodes.Create (4);
  
   CsmaHelper csma;
-  csma.SetChannelAttribute ("DataRate", DataRateValue (DataRate (5000000)));
+   //csma.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+  //csma.SetChannelAttribute ("Delay", StringValue ("2ms"));
+ csma.SetChannelAttribute ("DataRate", DataRateValue (DataRate (5000000)));
   csma.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (2)));
   csma.SetDeviceAttribute ("Mtu", UintegerValue (1400));
   //mtu->maximun transmission unit 
@@ -30,20 +32,19 @@ int main (int argc, char *argv[])
   Ipv4InterfaceContainer interface = address.Assign (device);
       //serverAddress = Address(i.GetAddress (1));
    
- // uint16_t port = 9;  // well-known echo port number
+ 
   UdpEchoServerHelper server (9);
   ApplicationContainer apps = server.Install (nodes.Get (1));
   apps.Start (Seconds (1.0));
   apps.Stop (Seconds (10.0));
 
 
-  //uint32_t packetSize = 1024;
-  //uint32_t maxPacketCount = 1;
-  //Time interPacketInterval = Seconds (1.);
+  
   UdpEchoClientHelper client (interface.GetAddress(1),9);
   client.SetAttribute ("MaxPackets", UintegerValue (1));
   client.SetAttribute ("Interval", TimeValue (Seconds(1.0)));
   client.SetAttribute ("PacketSize", UintegerValue (1024));
+
   apps = client.Install (nodes.Get (0));
   apps.Start (Seconds (2.0));
   apps.Stop (Seconds (10.0));
