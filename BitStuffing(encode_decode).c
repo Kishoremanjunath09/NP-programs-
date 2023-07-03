1e3bb6be301d90b3ed55221bca0ac083617305d6
Kishoremanjunath09 <116911063+Kishoremanjunath09@users.noreply.github.com>
2023-07-03 20:53:44 +0530

    Update BitStuffing(encode_decode).c

--- BitStuffing(encode_decode).c
+++ BitStuffing(encode_decode).c
@@ -1,94 +1,79 @@
+//bit stuffing(encode and decode)
 #include <stdio.h>
+int data[100];
 
-void bitStuffing(int arr[], int n)
-{
-    int count1 = 0;// to count no of 1's
-    int k = 0;//new array index
-    int array[20];//new output(stuffed) array
-
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] == 1)
-        {
+void bitstuffing(int arr[],int n){
+    int array[20];
+    int count1=0,k=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==1){
             count1++;
-            array[k] = arr[i];
+            array[k]=arr[i];
             k++;
-            //if a zero is followed by 5 1's then append 0 -->bit stuffing
-            if (count1 == 5 && arr[i - 5] == 0)
-            {
-                array[k] = 0;
+            if(count1==5 && arr[i-5]==0){
+                array[k]=0;
                 k++;
             }
         }
-        else
-        {
-            count1 = 0;
-            array[k] = arr[i];
+        else{
+            count1=0;
+            array[k]=arr[i];
             k++;
         }
     }
-
-    printf("Bit-stuffed array: ");
-    for (int a = 0; a < k; a++)
-    {
-        printf("%d ", array[a]);
-    }
-    printf("\n");
+        for(int i=0;i<k;i++){
+            data[i]=array[i];
+        }
+        printf("the bitstuffed data is :");
+        for(int i=0;i<k;i++){
+            printf("%d",data[i]);
+        }
+        printf("\n");
+         
+    
+    
 }
 
-void bitUnstuffing(int arr[], int n)
-{
-    int count1 = 0;
-    int k = 0;
+void bitunstuffing(int arr[],int n){
     int array[20];
-
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] == 1)
-        {
+    int count1=0,k=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==1){
             count1++;
-            array[k] = arr[i];
+            array[k]=arr[i];
             k++;
-            //if a zero is followed by 5 1's, skip the appended 0 -->bit unstuffing
-            if (count1 == 5 && arr[i-5]==0 && arr[i + 1] == 0)
-            {
+            if(count1==5 &&arr[i-5]==0 && arr[i+1]==0){
+                //array[k]=0;
+                //k++
                 i++;
             }
         }
-        else
-        {
-            count1 = 0;
-            array[k] = arr[i];
+        else{
+            count1=0;
+            array[k]=arr[i];
             k++;
         }
     }
-
-    printf("Bit-unstuffed array: ");
-    for (int a = 0; a < k; a++)
-    {
-        printf("%d ", array[a]);
-    }
-    printf("\n");
+        printf("the unbitstuffed data is :");
+        for(int i=0;i<k;i++){
+            printf("%d",array[i]);
+        }
+        printf("\n");
+       
 }
 
 int main()
 {
     int n;
-
-    printf("Enter the size of the bit string to be sent: ");
-    scanf("%d", &n);
-
-    int arr[n];
-
-    printf("Enter the bit string of length %d: ", n);
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &arr[i]);
+    printf("enter the no of bits:");
+    scanf("%d",&n);
+    int data[n];
+    printf("enter the data:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&data[i]);
     }
-
-    bitStuffing(arr, n);//encoding
-    printf("----------------------------------------------------------------\n");
-    bitUnstuffing(arr, n);//decoding
-
+    bitstuffing(data,n);
+    
+    bitunstuffing(data,n);
     return 0;
 }
