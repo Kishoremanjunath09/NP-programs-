4c59f4061ba77fce347735410ac2f6d239e07e11
Kishoremanjunath09 <116911063+Kishoremanjunath09@users.noreply.github.com>
2023-07-03 22:38:54 +0530

    Update ByteStuffing.c

--- ByteStuffing.c
+++ ByteStuffing.c
@@ -1,25 +1,27 @@
-#include <stdio.h>
-#include <string.h>
 
+#include <stdio.h>
+#include<string.h>
 int main()
 {
-    int n;
-    printf("enter number of frames");
-    scanf("%d", &n);
+    int n,len=0;
+    printf("enter the no of frames:");
+    scanf("%d",&n);
     char str[n][100];
-    int len;
-    char leng[10], output[1000];
-    for(int i=0; i<n; i++)
-    {
-        printf("enter %d frame bits", i+1);
-        scanf("%s", str[i]);
-        len = strlen(str[i]);
-        sprintf(leng, "%d", len);
-        strcat(output, str[i]);
-        strcat(output, leng);
+    char op[100], conv[100];
+    for(int i=0;i<n;i++){
+        printf("enter the frame %d",i+1);
+        scanf("%s",str[i]);
+        len=strlen(str[i]);
+        sprintf(conv,"%d",len);
+        strcat(str[i],conv);
+        strcat(op,str[i]);
+        
     }
-    printf("1000001-");
-    printf("%s", output);
-    printf("-1000001");
+    printf("the byte stuffed data is:\n");
+    printf("10000001-");
+    printf("%s",op);
+    printf("-10000001");
+    
+
     return 0;
 }
